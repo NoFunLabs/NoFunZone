1e387261416cebb81b9b8889aa25d8f81eacc1a7
Mark Steverberg <evan@fort1e.com>
2023-02-04 00:02:21 -0500

    test page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+
+import Navbar from './components/navbar/Navbar'
+import Main from './pages/main/Main'
+import Footer from './components/footer/Footer'
+
+//E~ Added for background implementation
+import Background from './components/background/Background'
+import { Switch, BrowserRouter, Route, Routes } from 'react-router-dom';
+import AvatarPage from './pages/avatarpage/AvatarPage';
+import MintPage from './pages/mintpage/MintPage';
 
 ReactDOM.render(
-     <React.StrictMode>
-          <App />
-     </React.StrictMode>,
+     <BrowserRouter>
+          <Background />
+          <Navbar />
+          <Routes>
+               <Route exact path="/" element={<Main />} />
+               <Route exact path="/mint" element={<MintPage />} />
+               <Route exact path="/avatar" element={<AvatarPage />} />
+               <Route exact path="/test" element={<MintPage />} />
+          </Routes>
+          <Footer />
+          </BrowserRouter>,
      document.getElementById('root')
 )
